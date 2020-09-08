9ab84ddca64a28b365d83c392f42d85855e1386a
Robert Yang <2285943+radiatoryang@users.noreply.github.com>
2020-09-08 18:23:58 -0400

    week02_introToCodeAndSprites: HelloWorld.cs, TextGame.cs, KeyboardMove.cs

---
+++ week02_introToCodeAndSprites/Assets/KeyboardMove.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardMove : MonoBehaviour
+{
+    void Update()
+    {
+        // Four different ways to code movement
+
+        // if the player holds down UP ARROW, then move up
+        if ( Input.GetKey(KeyCode.UpArrow) ) {
+            GetComponent<Transform>().position += new Vector3(0f, 0.1f, 0f);
+        }
+
+        // if the player holds down RIGHT ARROW, then move right
+        if ( Input.GetKey(KeyCode.RightArrow)) {
+            // "transform" is a shortcut for "GetComponent<Transform>()"
+            transform.position += new Vector3( 0.1f, 0f, 0f );
+        }
+
+        // etc. for DOWN ARROW, move down
+        if ( Input.GetKey(KeyCode.DownArrow) ) {
+            // if you don't need to do extra math with a position, and just want to
+            // do the equilvalent of a "+=" type of thing, then Translate() is good too
+            transform.Translate(0f, -0.1f, 0f);
+        }
+
+        // etc. for LEFT ARROW, move left
+        if ( Input.GetKey(KeyCode.LeftArrow) ) {
+            // if a GameObject has no parents, then "position" and "localPosition" are the same
+            transform.localPosition += new Vector3( -0.1f, 0f, 0f );
+        }
+    }
+}
---
+++ week02_introToCodeAndSprites/Assets/TextGame.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextGame : MonoBehaviour
+{
+    // declare some variables to keep track of our score
+    int myScore = 0;
+    public int scoreVictory = 20; // if a variable is "public" then it appears in the Inspector
+
+    void Update()
+    {
+        // press SPACE to get points
+        if ( Input.GetKeyDown(KeyCode.Space) ) { // KeyCode is an "enum"
+            myScore = myScore + 1; // other ways of doing the same thing: myScore +=1; myScore++;
+            Debug.Log( "myScore: " + myScore.ToString() );
+        }
+
+        // does the player have 20 points? if so, then they win!!
+        if ( myScore >= scoreVictory) {
+            Debug.Log("YOU WON!!!");
+        }
+
+    }
+}

d3be7db8f78006ba1d81c9be32884463aa34c707
Robert Yang <2285943+radiatoryang@users.noreply.github.com>
2020-09-22 17:51:24 -0400

    week04: added PlatformerCharacter.cs code

---
+++ week04_2dPhysicsCode/Assets/Scripts/PlatformerCharacter.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// PURPOSE: character controller with basic movement and jumping
+// USAGE: put this on an object with a 2D collider and 2D rigidbody
+public class PlatformerCharacter : MonoBehaviour
+{
+    // object references
+    Rigidbody2D myRigidbody; // stores a reference to the Rigidbody
+
+    // object state
+    float inputHorizontal;
+    bool isJumping;
+    public bool isGrounded; // set by PlatformerGroundedTrigger.cs
+
+    // tuning variables
+    public float moveSpeed = 15f, jumpPower = 25f;
+    
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>(); // cache the reference in Start to save us time and processing
+    }
+
+    // Update happens every RENDERING frame, this is also where input happens
+    void Update()
+    {
+        // get left/right arrow keys to move left/right with a virtual joystick
+        inputHorizontal = Input.GetAxis("Horizontal"); // a value from -1 to 1... -1 if left, +1 if right
+
+        // press SPACE to jump... but only if we're standing on the ground
+        if ( Input.GetButtonDown("Jump") && isGrounded == true ) {
+            isJumping = true;
+        }
+    }
+
+    // FixedUpdate happens every PHYSICS frame, at a fixed timestep
+    // (technically, everything that happens here is framerate independent, so no real need for Time.deltaTime here)
+    void FixedUpdate() {
+        // we need to put the Y value back into itself to preserve vertical velocity (e.g. falling)
+        myRigidbody.velocity = new Vector2( inputHorizontal * moveSpeed, myRigidbody.velocity.y );
+
+        // if we need to do the jump, then add Y velocity
+        if ( isJumping ) {
+            myRigidbody.velocity = new Vector2( myRigidbody.velocity.x, jumpPower );
+            isJumping = false;
+        }
+    }
+}
---
+++ week04_2dPhysicsCode/Assets/Scripts/PlatformerGroundedTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformerGroundedTrigger : MonoBehaviour
+{
+    // we need a reference to the PlatformerCharacter script, or else we can't talk to it
+    public PlatformerCharacter myCharacter; // public: assign in Inspector!
+
+    // happens EVERY FRAME there's an object inside this trigger area
+    void OnTriggerStay2D(Collider2D activator) {
+        myCharacter.isGrounded = true;
+    }
+
+    // happens the FIRST FRAME when an object leaves the trigger area
+    void OnTriggerExit2D(Collider2D activator) {
+        myCharacter.isGrounded = false;
+    }
+}

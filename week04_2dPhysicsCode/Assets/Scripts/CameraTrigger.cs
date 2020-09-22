5a9db216c6430fa0819684f38ab0846e610c99dc
Robert Yang <2285943+radiatoryang@users.noreply.github.com>
2020-09-22 16:09:34 -0400

    week04: see ProjectileLaunch.cs code and CameraTrigger.cs (smooth camera code)

---
+++ week04_2dPhysicsCode/Assets/Scripts/CameraTrigger.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// put this script on our trigger?
+public class CameraTrigger : MonoBehaviour
+{
+    Transform cameraFocusTarget;
+
+    void OnTriggerEnter2D(Collider2D activator) {
+        cameraFocusTarget = activator.transform;
+    }
+
+    void OnTriggerExit2D(Collider2D activator) {
+        cameraFocusTarget = null;
+    }
+
+    void Update () {
+        if ( cameraFocusTarget != null ) {
+
+            // Method A: manually move the Camera position towards the Camera target
+            Vector3 moveVector = cameraFocusTarget.position - Camera.main.transform.position;
+            if ( moveVector.magnitude > 1f ) {
+                moveVector = moveVector.normalized;
+            }
+            moveVector.z = 0f; // oops this is the bug I didn't fix during class
+            Camera.main.transform.position += moveVector * Time.deltaTime * 20f;
+
+            // Method B: or have Unity calculate all of the above for you
+            // Camera.main.transform.position = Vector2.MoveTowards( 
+            //     Camera.main.transform.position, 
+            //     cameraFocusTarget.position, 
+            //     Time.deltaTime * 20f
+            // );
+        }
+    }
+
+}
---
+++ week04_2dPhysicsCode/Assets/Scripts/ProjectileLaunch.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// PURPOSE: let me use the mouse to aim a cannon, 
+// and left-click to shoot a projectile ball
+
+// USAGE: put this script on pivot point parent,
+// with a long sprite child pointing downwards
+
+public class ProjectileLaunch : MonoBehaviour
+{
+    public float rotationSpeed = 90f; // public = exposed to Unity inspector
+    public float shootForce = 1000f; 
+    public Rigidbody2D myProjectilePrefab; // public = in Inspector, Rigidbody2D = for our convenience
+
+    void Update()
+    {
+        // move the mouse to aim the cannon
+        // -1f: moving mouse left, 0f: mouse not moving horizontal, +1.0f: moving mouse right
+        float horizontalMouseSpeed = Input.GetAxis("Mouse X");
+
+        // transform.Rotate( 0, 0, horizontalMouseSpeed); // this is FRAMERATE DEPENDENT
+        transform.Rotate( 0, 0, horizontalMouseSpeed * Time.deltaTime * rotationSpeed); // FRAMERATE INDEPENDENT because we use Time.deltaTime
+        // 60 FPS? dT = 1/60th of a second
+        // 1 FPS? dT = 1/1 = 1 second
+        // 1000 FPS? dT = 1/1000th of a second
+        // dT: usually a very small number between 0.0 - 1.0
+
+        // how to instantiate ("clone") an object
+        // 1. make a prefab object
+        // 2. declare and assign a public var to reference that prefab
+        // 3. use the Instantiate function
+
+        // left-click to shoot a projectile out of the cannon
+        if ( Input.GetMouseButtonDown(0) ) { // 0 = left-click, 1 = right-click, 2 = middle-click
+            Rigidbody2D myNewClone = Instantiate( myProjectilePrefab, transform.position - transform.up * 2, Quaternion.identity );
+            // add force in the direction of this cannon's current "down" vector
+            myNewClone.AddForce( -transform.up * shootForce );
+        }
+
+    }
+
+}
